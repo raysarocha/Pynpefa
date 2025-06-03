4961220668be3f7bec0c121ac75321da1764bc19
raysarocha <73074939+raysarocha@users.noreply.github.com>
2025-06-03 13:45:08 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyNPEFA',
+    version='0.1',
+    packages=['pynpefa'],
+    install_requires=['numpy', 'pandas', 'matplotlib'],
+    url='https://github.com/raysarocha/Pynpefa',
+    author='Ray S. Rocha',
+    author_email='',
+    description='Python implementation of NPEFA',
+)
